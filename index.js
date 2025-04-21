600ba452e0ee15a6ef4f26b5513215f5479af55a
pxlkeshi <101736130+pxlkeshi@users.noreply.github.com>
2025-04-21 15:04:27 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let runnerReg = true;
+runnerAge = 18;
+if (runnerReg && runnerAge > 18) {
+  raceNumber += 1000;
+}
+if (runnerReg && runnerAge > 18) {
+  console.log(`You will race at 9:30 am and your race number is ${raceNumber}`);
+} else if ((runnerReg = false && runnerAge > 18)) {
+  console.log(`You will race at 11:00am and your race number is ${raceNumber}`);
+} else if (runnerAge < 18) {
+  console.log(`you will race at 12:30pm and your race number is ${raceNumber}`);
+} else {
+  console.log("Please go see the registration desk");
+}
